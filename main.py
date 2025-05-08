7e3ce2d913847f3a903703df4739740d73ea1bd7
umutozgurdev <umutozgurdev@gmail.com>
2025-05-08 13:02:56 +0300

    refactor: move main logic to main.py

--- main.py
+++ main.py
@@ -1,2 +1,29 @@
-from stats import num_words
+import sys
+from stats import sort_on, dic_char, num_words
 
+def get_book_text(path_to_file):
+    with open(path_to_file) as f:
+        return f.read()
+
+def fancy_print(chars, wordnum, text):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {text}")
+    print("----------- Word Count ----------")
+    print(f"Found {wordnum} total words")
+    print("--------- Character Count -------")
+    sorted_chars = sorted(chars.items(), reverse=True, key=sort_on)
+    for char, count in sorted_chars:
+        if char.isalpha():
+            print(f"{char}: {count}")
+    print("============= END ===============")
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
+    wordnum = num_words(get_book_text(book_path))
+    chardic = dic_char(get_book_text(book_path))
+    fancy_print(chardic, wordnum, book_path)
+
+main()
--- stats.py
+++ stats.py
@@ -1,5 +1,3 @@
-import sys
-
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         return f.read()
@@ -24,27 +22,3 @@ def dic_char(text):
         else: 
             chars[char] = 1
     return chars
-
-def fancy_print(chars, wordnum, text):
-    print("============ BOOKBOT ============")
-    print(f"Analyzing book found at {text}")
-    print("----------- Word Count ----------")
-    print(f"Found {wordnum} total words")
-    print("--------- Character Count -------")
-    sorted_chars = sorted(chars.items(), reverse=True, key=sort_on)
-    for char, count in sorted_chars:
-        if char.isalpha():
-            print(f"{char}: {count}")
-    print("============= END ===============")
-
-def main():
-    if len(sys.argv) != 2:
-        print("Usage: python3 main.py <path_to_book>")
-        sys.exit(1)
-    book_path = sys.argv[1]
-    wordnum = num_words(get_book_text(book_path))
-    chardic = dic_char(get_book_text(book_path))
-    fancy_print(chardic, wordnum, book_path)
-
-main()
-
